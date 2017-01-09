7744741faed000b3cea82d099696361d1e25ffe1
charlotte <charlotte.angebert@gmail.com>
2017-01-09 17:19:08 +0100

    delete: divider

--- client/src/grid/Grid.jsx
+++ client/src/grid/Grid.jsx
@@ -15,7 +15,7 @@ class Grid extends React.Component {
   render () {
     return (
       <div>
-        <div className="divider"></div>
+       
         <div className="row">
           {
             this.state.names.map((n)=> {
